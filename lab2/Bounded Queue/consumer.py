e5456dcc6b7b757a3b680d65b455161e8ec51189
minako-aino <nastiadkpi@gmail.com>
2024-03-26 15:19:41 +0200

    lab2 - finishing touches

--- lab2/Bounded Queue/consumer.py
+++ lab2/Bounded Queue/consumer.py
@@ -1,11 +1,18 @@
 import hazelcast
 
-client = hazelcast.HazelcastClient(cluster_name="lab2")
-queue = client.get_queue("queue").blocking()
+CLUSTER_NAME = "lab2"
+QUEUE_NAME = "queue"
 
-while True:
-    item = queue.take()
-    print("Consumed " + str(item))
-    if (item == -1):
-        queue.put(-1)
-        break
+client = hazelcast.HazelcastClient(cluster_name=CLUSTER_NAME)
+queue = client.get_queue(QUEUE_NAME).blocking()
+
+try:
+    while True:
+        consumed_item = queue.take()
+        print(f"Consumed {consumed_item}")
+
+        if consumed_item == -1:
+            queue.put(-1)
+            break
+finally:
+    client.shutdown()
--- lab2/Bounded Queue/producer.py
+++ lab2/Bounded Queue/producer.py
@@ -1,27 +1,17 @@
 import hazelcast
 
-client = hazelcast.HazelcastClient(cluster_name="lab2")
-queue = client.get_queue("queue").blocking()
+CLUSTER_NAME = "lab2"
+QUEUE_NAME = "queue"
 
-for i in range(100):
-    queue.put(i)
-    print("Producing " + str(i))
-queue.put(-1)
-print("Producer finished")
-client.shutdown()
+client = hazelcast.HazelcastClient(cluster_name=CLUSTER_NAME)
 
-"""import hazelcast
+try:
+    queue = client.get_queue(QUEUE_NAME).blocking()
+    for i in range(100):
+        queue.put(i)
+        print(f"Producing {i}")
+    queue.put(-1)
+    print("Producer finished")
 
-
-client = hazelcast.HazelcastClient(cluster_name="lab2")
-
-queue = client.get_queue("queue").blocking()
-print('starting stuff')
-for i in range(1, 101):
-    queue.put(i)
-    print("Producing " + str(i))
-queue.put(-1)
-print("Producer finished")
-
-client.shutdown()
-"""
+finally:
+    client.shutdown()
